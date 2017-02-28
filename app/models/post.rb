6cdee9e6a9200f1b7a22b3bb8ad9d8848d696ca3
Yusuke <y.omt0601@gmail.com>
2017-02-28 14:06:38 +0000

    add models

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+  belongs_to :user
+  has_many :tags
+  validates :content, length: { maximum: 100}
+                      presence: ture
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :tags
+  resources :posts
+  resources :users
   root 'application#hello'
 end
