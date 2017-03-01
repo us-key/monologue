466e8d83f31bf80c0b9afc5221b3922bdc00cbbb
Yusuke <y.omt0601@gmail.com>
2017-03-01 23:46:04 +0000

    create signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  
+  get  '/signup',  to: 'users#new'
+
   resources :tags
   resources :posts
   resources :users
-  root 'application#hello'
+  root 'static_pages#home'
 end
