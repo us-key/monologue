87d5ddc226597cce03245468a4c7794901d32b3f
Yusuke <y.omt0601@gmail.com>
2017-03-02 10:35:57 +0000

    add password to user

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :tags
-  validates :content, length: { maximum: 100}
+  validates :content, length: { maximum: 100}, 
                       presence: ture
 end
