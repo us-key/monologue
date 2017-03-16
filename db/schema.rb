3184a753403c6e88da2369b736b83a0c2e8114e5
us-key <y.omt0601@gmail.com>
2017-03-16 19:28:54 +0900

    add password reset function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170309235140) do
+ActiveRecord::Schema.define(version: 20170315235029) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "content"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 20170309235140) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
