c872cc8c21ba095e54e3f694a59d1b80bdc65c0e
us-key <y.omt0601@gmail.com>
2017-03-27 09:01:29 +0900

    revice ui

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,11 @@ class StaticPagesController < ApplicationController
   skip_before_action :logged_in_user
 
   def home
+
+    # ログイン済みの場合Postsページにリダイレクトする
+    if logged_in?
+      redirect_to posts_url
+    end
   end
 
 end
