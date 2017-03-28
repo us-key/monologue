447ba6a974b2e9a136901b5ad7382a31ee2c29ca
us-key <y.omt0601@gmail.com>
2017-03-28 23:30:52 +0900

    #1 add tag function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -101,6 +101,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:content, :user_id)
+      params.require(:post).permit(:content, :user_id, :label_list)
     end
 end
